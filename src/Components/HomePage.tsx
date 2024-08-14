df1b581ef7868a8bb59e8829db45073235be8549
Unnat Agrawal <agrawalunnat7779@gmail.com>
2024-08-14 23:24:50 +0530

    skills added

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -5,6 +5,7 @@ import Mail from './Mail'
 import About from './About'
 import Projects from './Projects'
 import Experience from './Experience'
+import Skills from './Skills'
 const Homepage = () => {
   return (
     <div>
@@ -13,8 +14,9 @@ const Homepage = () => {
         <Projects/>
         <Social />
         <Mail />
-        <Experience/>
         
+        <Skills/> 
+        <Experience/>
 
     </div>
   )
